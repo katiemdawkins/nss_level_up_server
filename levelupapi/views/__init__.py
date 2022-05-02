4bfe9950728d82b1cc4975d615af2c6d1e91a159
Katie Dawkins <katiemdawkins@gmail.com>
2022-05-02 15:47:07 -0500

    added to database and created Views etc

---
+++ levelupapi/views/__init__.py
@@ -0,0 +1,7 @@
+from .auth import login_user, register_user
+from .game_type import GameTypeView
+from .game_type import GameTypeSerializer
+from .EventView import EventView
+from .EventView import EventSerializer
+from .GameView import GameSerializer
+from .GameView import GameView
